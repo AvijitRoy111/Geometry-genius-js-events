b0f1d2df48da29a461fe7b639c5c06aafcab9116
unknown <AvijitRoy111>
2024-09-23 00:09:21 +0600

    added the change section

--- scripts/Geometry.js
+++ scripts/Geometry.js
@@ -43,19 +43,94 @@ function calculateReactraingleArea(){
 
 // Parallelogram style
 
+// function calculateParallelogramArea(){
+//     // Parallelogram-base
+//     const pallalelogramBaseInput = document.getElementById('parallelogram-base');
+//     const pallalelogramBaseText = pallalelogramBaseInput.value;
+//     const parallelogramBase = parseFloat(pallalelogramBaseText);
+
+//     // Parallelogram-hight 
+//     const pallalelogramHightInput = document.getElementById('parallelogram-hight');
+//     const pallalelogramHightText = pallalelogramHightInput.value;
+//     const parallelogramHight = parseFloat(pallalelogramHightText) ;
+
+//     const pallalelogramResult = parallelogramBase * parallelogramHight ;
+
+//     const pallalelogramAreas = document.getElementById('pallalelogram-areas');
+//     pallalelogramAreas.innerText = pallalelogramResult ;
+// }
+
+// no reapet and shortcut rule
 function calculateParallelogramArea(){
-    // Parallelogram-base
-    const pallalelogramBaseInput = document.getElementById('parallelogram-base');
-    const pallalelogramBaseText = pallalelogramBaseInput.value;
-    const parallelogramBase = pallalelogramBaseText;
+    const base = getInputValueById('parallelogram-base');
+    const hight = getInputValueById('parallelogram-hight');
+    const area = base * hight ;
 
-    // Parallelogram-hight 
-    const pallalelogramHightInput = document.getElementById('parallelogram-hight');
-    const pallalelogramHightText = pallalelogramHightInput.value;
-    const parallelogramHight = pallalelogramHightText;
+    setInnerTextById('pallalelogram-areas', area)
+}
+function getInputValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputValueText = inputField.value;
+    const inputValue = parseFloat (inputValueText);
+    return inputValue;
+}
+function setInnerTextById(elementId, area){
+    const element = document.getElementById(elementId);
+    element.innerText = area ;
+}
 
-    const pallalelogramResult = parallelogramBase * parallelogramHight ;
 
-    const pallalelogramAreas = document.getElementById('pallalelogram-areas');
-    pallalelogramAreas.innerText = pallalelogramResult ;
+// RombosArea
+
+function calculateRhombusArea(){
+    // Rombos first Corno input
+    const rombosFirstKornoInput = document.getElementById('rombos-first-korno');
+    const rombosFirstkornoText = rombosFirstKornoInput.value;
+    const firstCorno = parseFloat(rombosFirstkornoText) ;
+    // Rombos second Corno input
+    const rombosSecondCornoInput = document.getElementById('rombos-second-korno');
+    const rombosSeconCornoText = rombosSecondCornoInput.value;
+    const secondCorno = parseFloat(rombosSeconCornoText) ;
+    // Rombos Formulla
+    const rombosArea = 0.5 * firstCorno * secondCorno ;
+    // Sent Area in Areas
+    const rombosAreas = document.getElementById('rombos-areas');
+    rombosAreas.innerText = rombosArea ;
+}
+
+
+// PentagonArea
+function calculatePentagonArea(){
+    // Pentagon input
+    const pentagonPoridhiInput = document.getElementById('pentagon-poridhi');
+    const pentagonPoridhiText = pentagonPoridhiInput.value;
+    const poridhi = parseFloat( pentagonPoridhiText);
+    //Pentagon input
+    const pentagonBaseInput = document.getElementById('pentagon-base');
+    const pentagonBaseText = pentagonBaseInput.value;
+    const base =parseFloat(pentagonBaseText);
+    //pentagon Formulla
+    const pentagonArea = 0.5 * poridhi * base ;
+    // Sent Area in Areas
+    const pentagonAreas = document.getElementById('pentagon-areas');
+    pentagonAreas.innerText = pentagonArea ;
+}
+
+
+
+//  EllipseArea
+function calculateEllipsArea(){
+    // Ellipse area input
+    const ellipsAreaInput = document.getElementById('ellips-Area');
+    const ellipsAreaText = ellipsAreaInput.value;
+    const area = parseFloat(ellipsAreaText) ;
+    // Ellipse base input
+    const ellipsBaseInput = document.getElementById('ellips-base');
+    const ellipsBaseText = ellipsBaseInput.value;
+    const base = parseFloat(ellipsBaseText) ;
+    // Ellips Formulla
+    const ellipsResult = 3.1416 * area * base ;
+    // Sent Area in Areas
+    const ellipsAreas = document.getElementById('ellips-result');
+    ellipsAreas.innerText = ellipsResult ;
 }
